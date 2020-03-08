702a37fda683f4dc1552732fb311ea53bff03ecd
Frans Dekkers <fde.gma@gmail.com>
2020-03-08 12:24:41 +0100

    Small corrections to deletePerson

--- api/api/controllers/delete-person.js
+++ api/api/controllers/delete-person.js
@@ -2,80 +2,81 @@
 module.exports = {
 
 
-        friendlyName: 'Remove a Person',
+    friendlyName: 'Remove a Person',
 
 
-        description: 'Based on a givven PersonID, MotherID, FatherID and PartnerID remove a person and his/her parents and partner form the database.',
+    description: 'Based on a givven PersonID, MotherID, FatherID and PartnerID remove a person and his/her parents and partner form the database.',
 
 
-        inputs: {
-            PersonID: {
-                description: 'The Id of the Person to be removed from the parent.',
-                type: 'number',
-                required: true
-            },
-            MotherID: {
-                description: 'The id of the Mother to be removed from the Person.',
-                type: 'number',
-                required: true
-            },
-            FatherID: {
-                description: 'The id of the Father to be removed from the Person.',
-                type: 'number',
-                required: true
-            },
-            PartnerID: {
-                description: 'The id of the Partner to be removed from the Person.',
-                type: 'number',
-                required: true
-            },
-            Timestamp: {
-                description: 'The timestamp of when the record in the database was added or last changed.',
-                type: 'Date',
-                required: true
-            },
+    inputs: {
+        PersonID: {
+            description: 'The Id of the Person to be removed from the parent.',
+            type: 'number',
+            required: true
+        },
+        MotherID: {
+            description: 'The id of the Mother to be removed from the Person.',
+            type: 'number',
+            required: true
+        },
+        FatherID: {
+            description: 'The id of the Father to be removed from the Person.',
+            type: 'number',
+            required: true
+        },
+        PartnerID: {
+            description: 'The id of the Partner to be removed from the Person.',
+            type: 'number',
+            required: true
+        },
+        Timestamp: {
+            description: 'The timestamp of when the record in the database was added or last changed.',
+            type: 'string',
+            required: true
+        },
+    },
 
 
-            exits: {
+    exits: {
 
-                success: {
-                    description: 'Person was removed',
-                    responseType: ''
-                },
+        success: {
+            description: 'Person was removed',
+            responseType: ''
+        },
 
-                noSuccess: {
-                    description: 'Person was NOT removed from parent.',
-                    responseType: ''
-                },
+        noSuccess: {
+            description: 'Person was NOT removed from parent.',
+            responseType: ''
+        },
 
-                notExecuted: {
-                    description: 'Person was NOT removed from parent.',
-                    responseType: 'notFound'
-                }
-
-
-            },
+        notExecuted: {
+            description: 'Person was NOT removed from parent.',
+            responseType: 'notFound'
+        }
 
 
-            fn: async function(inputs, exits) {
-                var actionResult = await sails.sendNativeQuery('call deletePerson($1, $2, $3, $4, $5)', [inputs.PersonID, inputs.MotherID, inputs.FatherID, inputs.ParentID, inputs.Timestamp]);
-                if (actionResult.rows[0].length === 0) {
-                    return exits.notExecuted({
-                        message: 'Person' + inputs.PersonID + ' was not deleted'
+    },
+
+
+    fn: async function(inputs, exits) {
+        var actionResult = await sails.sendNativeQuery('call deletePerson($1, $2, $3, $4, $5)', [inputs.PersonID, inputs.MotherID, inputs.FatherID, inputs.ParentID, inputs.Timestamp]);
+        if (actionResult.rows[0].length === 0) {
+            return exits.notExecuted({
+                message: 'Person' + inputs.PersonID + ' was not deleted'
+            });
+        } else {
+            switch (actionResult.rows[0][0].Result) {
+                case 'Person does not exist':
+                    return exits.noSuccess({
+                        message: 'Person ' + inputs.PersonID + ' was not deleted',
+                        data: actionResult.rows[0]
                     });
-                } else {
-                    switch (actionResult.rows[0][0].Result) {
-                        case 'Person does not exist':
-                            return exits.noSuccess({
-                                message: 'Person ' + inputs.PersonID + ' was not deleted',
-                                data: actionResult.rows[0]
-                            });
-                        case 'Relation removed':
-                            return exits.success({
-                                message: 'Person' + inputs.PersonID + ' was deleted',
-                                data: actionResult.rows[0]
-                            });
-                    }
-                }
+                case 'Relation removed':
+                    return exits.success({
+                        message: 'Person' + inputs.PersonID + ' was deleted',
+                        data: actionResult.rows[0]
+                    });
             }
-        };
+        }
+    }
+};
