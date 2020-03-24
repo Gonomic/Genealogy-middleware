6877cf729c6e6f157dc83ae6241342142a576f6c
Frans Dekkers <fde.gma@gmail.com>
2020-03-24 23:55:31 +0100

    Ready for docker

--- api/config/session.js
+++ api/config/session.js
@@ -11,29 +11,34 @@
 
 module.exports.session = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
-  secret: '64a62e2959c0fbcaabf85f6f90845032',
+    /***************************************************************************
+     *                                                                          *
+     * Session secret is automatically generated when your new app is created   *
+     * Replace at your own risk in production-- you will invalidate the cookies *
+     * of your users, forcing them to log in again.                             *
+     *                                                                          *
+     ***************************************************************************/
+    secret: '64a62e2959c0fbcaabf85f6f90845032',
+
+    // FDE 22-3-2020
+    cookie: {
+        secure: true
+    }
 
 
-  /***************************************************************************
-  *                                                                          *
-  * Customize when built-in session support will be skipped.                 *
-  *                                                                          *
-  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
-  * session management when responding to simple requests for static assets, *
-  * like images or stylesheets.)                                             *
-  *                                                                          *
-  * https://sailsjs.com/config/session                                       *
-  *                                                                          *
-  ***************************************************************************/
-  // isSessionDisabled: function (req){
-  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
-  // },
+    /***************************************************************************
+     *                                                                          *
+     * Customize when built-in session support will be skipped.                 *
+     *                                                                          *
+     * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+     * session management when responding to simple requests for static assets, *
+     * like images or stylesheets.)                                             *
+     *                                                                          *
+     * https://sailsjs.com/config/session                                       *
+     *                                                                          *
+     ***************************************************************************/
+    // isSessionDisabled: function (req){
+    //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+    // },
 
 };
