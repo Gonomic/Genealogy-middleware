c5e690a629cc5e930cdd273a65522b516c8e46fd
Frans Dekkers <fde.gma@gmail.com>
2020-04-30 00:00:31 +0200

    Adatptions for local socket on dev machine

--- api/config/env/development.js
+++ api/config/env/development.js
@@ -252,17 +252,17 @@ module.exports = {
          * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
          *                                                                          *
          ***************************************************************************/
-        // onlyAllowOrigins: [
-        //     'https://dekknet.com',
-        //     'https://dekknet.com:1001',
-        //     'https://dekknet.com:1002',
-        //     'https://Localhost',
-        //     'https://Localhost:1001',
-        //     'https://Localhost:1002',
-        //     'https://192.168.1.11',
-        //     'https://192.168.1.11:1001',
-        //     'https://192.168.1.11:1002'
-        // ],
+        onlyAllowOrigins: [
+            'https://dekknet.com',
+            'https://dekknet.com:1001',
+            'https://dekknet.com:1002',
+            'http://localhost',
+            'http://localhost:1001',
+            'http://localhost:1002',
+            'http://192.168.1.11',
+            'http://192.168.1.11:1001',
+            'http://192.168.1.11:1002'
+        ],
 
 
         /***************************************************************************
